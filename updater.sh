7119d45d79b0a24a285f18862ad4dd59d717dae3
bbhtt <bbhtt.zn0i8@slmail.me>
2022-12-09 16:11:02 +0530

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -5,16 +5,16 @@ export MANIFEST_PATH=$CI_PROJECT_DIR/$APP_ID.yaml
 #export MANIFEST_PATH=org.mozilla.FirefoxNightly.yaml
 
 # Extract the filename for the old release
-sed '157!d' $MANIFEST_PATH|sed -n '{s|.*/firefox-\(.*\)\.tar.bz2|\1|p;q;}' > oldrelease;
+sed '155!d' $MANIFEST_PATH|sed -n '{s|.*/firefox-\(.*\)\.tar.bz2|\1|p;q;}' > oldrelease;
 
 # Extract the filename for the new release
 wget -nv --spider -S --max-redirect 0 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US" 2>&1 | sed -n '/Location: /{s|.*/firefox-\(.*\)\.tar.*|\1|p;q;}' > newrelease;
 
 # Replace the filename to update the url
-newrelease=$(cat newrelease) && oldrelease=$(cat oldrelease) && sed -i "157s/$oldrelease/$newrelease/g" $MANIFEST_PATH;
+newrelease=$(cat newrelease) && oldrelease=$(cat oldrelease) && sed -i "155s/$oldrelease/$newrelease/g" $MANIFEST_PATH;
 
 # Extract the filename again in case of an update
-sed '157!d' $MANIFEST_PATH|sed -n '{s|.*/firefox-\(.*\)\.tar.bz2|\1|p;q;}' > updrelease;
+sed '155!d' $MANIFEST_PATH|sed -n '{s|.*/firefox-\(.*\)\.tar.bz2|\1|p;q;}' > updrelease;
 
 # Download the latest version
 wget -nv https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central/firefox-$(cat updrelease).checksums;
@@ -22,10 +22,10 @@ wget -nv https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-m
 cat firefox-$(cat updrelease).checksums|grep "$(cat updrelease).tar.bz2"|grep -v asc|grep -v sha512|cut -d " " -f1 > shanew;
 
 # Calculate the old checksum
-sed '158!d' $MANIFEST_PATH|cut -d: -f2|sed 's/^[ \t]*//;s/[ \t]*$//' > shaold;
+sed '156!d' $MANIFEST_PATH|cut -d: -f2|sed 's/^[ \t]*//;s/[ \t]*$//' > shaold;
 
 # Replace the checksum
-shanew=$(cat shanew) && shaold=$(cat shaold) && sed -i "158s/$shaold/$shanew/g" $MANIFEST_PATH;
+shanew=$(cat shanew) && shaold=$(cat shaold) && sed -i "156s/$shaold/$shanew/g" $MANIFEST_PATH;
 
 # Delete the leftovers
 rm *release sha* *.checksums;
